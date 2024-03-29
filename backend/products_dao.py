6a1e1ef6203f9c15a1ad416c86cfb22a5623f4af
raphaeltaurai <shawntaurai@byui.edu>
2024-03-30 01:06:26 +0200

    adding product_dao to server

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -37,9 +37,10 @@ def delete_product(connection, product_id):
 
 if __name__ == '__main__':
     connection = get_sql_connection()
-    # print(get_all_products(connection))
-    print(insert_new_product(connection, {
-        'product_name': 'potatoes',
-        'uom_id': '1',
-        'price_per_unit': 10
-    }))
+    #print(get_all_products(connection))
+    # print(insert_new_product(connection, {
+    #     'product_name': 'potatoes',
+    #     'uom_id': '1',
+    #     'price_per_unit': 10
+    #     }))
+    print(delete_product(connection, 9))
